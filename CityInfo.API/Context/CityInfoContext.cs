6fd28fb24bd763104a8f00df250db6b5d497c682
VihagaYohan <vihagayohan94@gmail.com>
2026-07-04 12:24:59 +0530

    Added - Entity framework core integration

---
+++ CityInfo.API/Context/CityInfoContext.cs
@@ -0,0 +1,16 @@
+﻿using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API.Context
+{
+    public class CityInfoContext: DbContext
+    {
+        public CityInfoContext(DbContextOptions<CityInfoContext> options)
+            : base(options) 
+        {
+
+        }
+
+        public DbSet<City> Cities { get; set; }
+    }
+}
---
+++ CityInfo.API/Entities/City.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Entities
+{
+    public class City
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set;}
+    }
+}
