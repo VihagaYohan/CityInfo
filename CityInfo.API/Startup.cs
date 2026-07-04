311f60408e65c26ee843759338bfe522ef8c9bb0
VihagaYohan <vihagayohan94@gmail.com>
2026-07-04 11:31:27 +0530

    Added - status code pages middleware

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -40,6 +40,7 @@ namespace CityInfo.API
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 //app.UseHsts();
             }
+            app.UseStatusCodePages();
             app.UseMvc();
             //app.UseHttpsRedirection();
             app.UseStaticFiles();
